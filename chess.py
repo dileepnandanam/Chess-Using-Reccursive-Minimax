af21c49c1c3fe7809fbdbc98828dcdd1784d2969
dileepnandanam <dileepsnandanam@gmail.com>
2012-08-30 09:47:10 +0530

    basic structure

---
+++ chess.py
@@ -0,0 +1,77 @@
+import minmax
+import pygame
+import sys
+
+class chessgui:
+	def __init__(self):
+		pygame.init()
+		
+		self.bricksize=80
+		self.chesscanvas=pygame.display.set_mode((self.bricksize*8,self.bricksize*8))
+		
+		self.board=minmax.board()
+		
+		self.black=-1
+		self.white=1
+		self.pcolor=self.black
+		self.opcolor=self.white
+		
+		self.img=[[pygame.image.load("troop/"+i+j+".gif") for j in ("k","q","b","h","f","p")] for i in ("b","w")]
+		print self.img
+		
+		
+		
+		pygame.display.flip()
+		
+	def display(self,cell):
+		for i in range(8):
+			for j in range(8):
+				if i%2==0 and j%2==0 or i%2==1 and j%2==1:
+					pygame.draw.rect(self.chesscanvas, (225,225,225), ((i*self.bricksize,j*self.bricksize),(self.bricksize,self.bricksize)),0)
+				else:
+					pygame.draw.rect(self.chesscanvas, (0,100,225), ((i*self.bricksize,j*self.bricksize),(self.bricksize,self.bricksize)),0)
+		for i in range(8):
+			for j in range(8):
+				if not cell[i][j]==None:
+
+					self.chesscanvas.blit(self.img[(cell[i][j].color==self.pcolor and 0 or cell[i][j].color==self.opcolor and 1)][cell[i][j].rank-1], (j*self.bricksize+5,i*self.bricksize+5))		
+				
+		pygame.display.flip()
+			
+	def getcell(self,p):
+		return (p[1]/self.bricksize,p[0]/self.bricksize)
+			
+	def main(self):
+		selected=False
+		fr=(0,0)
+		to=(0,0)
+		cell=self.board.cell
+		while True:
+			self.display(cell)
+			for event in pygame.event.get():
+				if event.type==pygame.QUIT:
+					sys.exit()
+				if event.type==pygame.MOUSEBUTTONDOWN:
+					if selected:
+						to=self.getcell(pygame.mouse.get_pos())	
+						if not fr==to :
+							
+							if self.board.movable(cell,self.pcolor,fr[0],fr[1],to[0],to[1]):
+								print 'moving to', to								
+								self.board.move(cell,fr[0],fr[1],to[0],to[1])
+								self.display(cell)
+								cell=self.board.bestposition(cell,self.opcolor,3)
+								if cell==None or self.board.checkmate(cell,self.pcolor):
+									print 'checkmate'
+									self.board.boardinit()
+								else:
+									self.display(cell)
+							selected=False
+					else:
+						fr=self.getcell(pygame.mouse.get_pos())
+						if not cell[fr[0]][fr[1]]==None and cell[fr[0]][fr[1]].color==self.pcolor:
+							selected=True
+							print fr, 'selected'
+	
+gui=chessgui()
+gui.main()		
---
+++ minmax.py
@@ -0,0 +1,321 @@
+
+class troop:
+	def __init__(self,rank,color):
+		self.rank=rank
+		self.color=color
+		self.fighting=True
+		self.ontimeability=True
+			
+	
+			
+class board:
+	def __init__(self):
+		self.cell=[[None for i in range(8)] for j in range(8)]
+		
+		self.boardinit()
+		#self.testcase()
+		#self.maxforecast=3
+		
+		
+	def boardinit(self):		
+		self.cell[1][0]=troop(6,1)
+		self.cell[1][1]=troop(6,1)
+		self.cell[1][2]=troop(6,1)
+		self.cell[1][3]=troop(6,1)
+		self.cell[1][4]=troop(6,1)
+		self.cell[1][5]=troop(6,1)
+		self.cell[1][6]=troop(6,1)
+		self.cell[1][7]=troop(6,1)
+		self.cell[0][0]=troop(5,1)
+		self.cell[0][1]=troop(4,1)
+		self.cell[0][2]=troop(3,1)
+		self.cell[0][3]=troop(2,1)
+		self.cell[0][4]=troop(1,1)
+		self.cell[0][5]=troop(3,1)
+		self.cell[0][6]=troop(4,1)
+		self.cell[0][7]=troop(5,1)
+		
+		self.cell[6][0]=troop(6,-1)
+		self.cell[6][1]=troop(6,-1)
+		self.cell[6][2]=troop(6,-1)
+		self.cell[6][3]=troop(6,-1)
+		self.cell[6][4]=troop(6,-1)
+		self.cell[6][5]=troop(6,-1)
+		self.cell[6][6]=troop(6,-1)
+		self.cell[6][7]=troop(6,-1)
+		self.cell[7][0]=troop(5,-1)
+		self.cell[7][1]=troop(4,-1)
+		self.cell[7][2]=troop(3,-1)
+		self.cell[7][4]=troop(2,-1)
+		self.cell[7][3]=troop(1,-1)
+		self.cell[7][5]=troop(3,-1)
+		self.cell[7][6]=troop(4,-1)
+		self.cell[7][7]=troop(5,-1)
+	def testcase(self):
+			
+		self.cell[0][0]=troop(1,1)
+		self.cell[1][7]=troop(5,-1)
+		self.cell[7][6]=troop(2,-1)
+		
+		
+	
+	def inside(self,x,y):
+		if x>=0 and x<8 and y>=0 and y<8: 
+			return True
+		return False
+	
+	
+	def copy(self,cell):
+		return [[cell[j][i] for i in range(8)] for j in range(8)]
+	
+	
+	
+	def safe(self,i,j,cell,color):
+		
+		for (d1,d2) in ((1,1),(-1,-1),(-1,1),(1,-1)):
+			x=i
+			y=j
+			while True:
+				x+=d1
+				y+=d2
+				
+				if not self.inside(x,y):
+					break
+				elif not cell[x][y]==None:
+					if not cell[x][y].color==color and (cell[x][y].rank in (2,3)):
+						return False
+					break
+		for (d1,d2) in ((0,1),(1,0),(0,-1),(-1,0)):
+			x=i
+			y=j
+			while True:
+				x+=d1
+				y+=d2
+				
+				if not self.inside(x,y):
+					break
+				elif not cell[x][y]==None:
+					if not cell[x][y].color==color and (cell[x][y].rank in (2,5)):
+						return False
+					break
+		
+		for (d1,d2) in ((2,1),(-2,-1),(2,-1),(-2,1),(1,2),(-1,-2),(-1,2),(1,-2)):
+			if self.inside(i+d1,j+d2):	
+				if not cell[i+d1][j+d2]==None and not cell[i+d1][j+d2].color==color and cell[i+d1][j+d2].rank==4:
+					return False
+		for (d1,d2) in ((color,1),(color,-1)):
+			if self.inside(i+d1,j+d2):	
+				if not cell[i+d1][j+d2]==None and not cell[i+d1][j+d2].color==color and cell[i+d1][j+d2].rank==6:
+					return False
+		for (d1,d2) in ((-1,-1),(1,-1),(-1,1),(1,1),(0,1),(1,0),(0,-1),(-1,0)):
+			if self.inside(i+d1,j+d2):	
+				if not cell[i+d1][j+d2]==None and not cell[i+d1][j+d2].color==color and cell[i+d1][j+d2].rank==1:
+					return False
+		return True
+					
+	def coverage(self,i,j,cell):
+	
+		color=cell[i][j].color
+		rang=[]					
+		if cell[i][j].rank==1:
+			
+			for (d1,d2) in ((-1,-1),(1,-1),(-1,1),(1,1),(0,1),(1,0),(0,-1),(-1,0)):
+				if self.inside(i+d1,j+d2):	
+					if (cell[i+d1][j+d2]==None or not cell[i+d1][j+d2].color==color) and self.safe(i+d1,j+d2,cell,color):
+						rang.append((i+d1,j+d2))					
+		if cell[i][j].rank==2:
+			for (d1,d2) in ((0,1),(1,1),(1,0),(0,-1),(-1,-1),(-1,0),(-1,1),(1,-1)):
+				
+				x=i
+				y=j
+				while True:
+					
+					x+=d1
+					y+=d2
+					if not self.inside(x,y):
+						break
+					elif cell[x][y]==None:
+						rang.append((x,y))
+					else:
+						if not cell[x][y].color==color:
+							rang.append((x,y))
+						break
+							
+						
+		
+		if cell[i][j].rank==3:
+		
+			for (d1,d2) in ((1,1),(-1,-1),(-1,1),(1,-1)):
+				
+				x=i
+				y=j
+				while True:
+					
+					x+=d1
+					y+=d2
+					if not self.inside(x,y):
+						break
+					elif cell[x][y]==None:
+						rang.append((x,y))
+					else:
+						if not cell[x][y].color==color:
+							rang.append((x,y))
+						break
+			
+		if cell[i][j].rank==4:
+			for (d1,d2) in ((2,1),(-2,-1),(2,-1),(-2,1),(1,2),(-1,-2),(-1,2),(1,-2)):
+				if self.inside(i+d1,j+d2):	
+					if cell[i+d1][j+d2]==None:
+						rang.append((i+d1,j+d2))
+					elif not cell[i+d1][j+d2].color==color:
+						rang.append((i+d1,j+d2))
+		if cell[i][j].rank==5:
+			for (d1,d2) in ((0,1),(1,0),(0,-1),(-1,0)):
+				
+				x=i
+				y=j
+				while True:
+					
+					x+=d1
+					y+=d2
+					if not self.inside(x,y):
+						break
+					elif cell[x][y]==None:
+						rang.append((x,y))
+					else:
+						if not cell[x][y].color==color:
+							rang.append((x,y))
+						break
+		if cell[i][j].rank==6:
+			if self.inside(i+cell[i][j].color,j):
+				if cell[i+cell[i][j].color][j]==None:
+					rang.append((i+cell[i][j].color,j))
+					if cell[i][j].color==-1 and i==6 or cell[i][j].color==1 and i==1:
+						if cell[i+cell[i][j].color*2][j]==None:
+							rang.append((i+cell[i][j].color*2,j))
+				
+			if self.inside(i+cell[i][j].color,j+1) and not cell[i+cell[i][j].color][j+1]==None and not cell[i+cell[i][j].color][j+1].color==color:
+				rang.append((i+cell[i][j].color,j+1))
+			if self.inside(i+cell[i][j].color,j-1) and not cell[i+cell[i][j].color][j-1]==None and not cell[i+cell[i][j].color][j-1].color==color:
+				rang.append((i+cell[i][j].color,j-1))
+		return rang	
+	def move(self,cell,i,j,x,y):
+		cell[x][y]=cell[i][j]
+		cell[i][j]=None
+		return cell
+	def movable(self,cell,color,i,j,x,y):
+		if not cell[x][y]==None and cell[x][y].color==color:
+			return False
+		
+		return((x,y) in self.coverage(i,j,cell))
+	def force(self,color,cell):
+		l=[]
+		for i in range(8):
+			for j in range(8):
+				if not cell[i][j]== None and cell[i][j].color==color:
+					l.append((i,j))
+		return l 
+	def power(self,color,cell):
+		p=0
+		for (x,y) in self.force(color,cell):
+			p+=7-cell[x][y].rank
+		
+		return p
+	def profit(self,cell,color,pcolor,pforce,oforce,depth):
+		npforce=self.power(pcolor,cell)
+		noforce=self.power(pcolor*-1,cell)
+		
+		if depth==0:
+			
+			return (npforce-pforce+oforce-noforce)*100
+			
+
+		for (x,y) in self.force(color,cell):
+			
+			if cell[x][y].rank==1:
+				
+				if not self.safe(x,y,cell,color):
+					
+					if len(self.coverage(x,y,cell))==0:
+						
+						return (color==pcolor and -100 or 100)*depth*100
+						
+					possible=self.coverage(x,y,cell)
+					c=0
+					for (p,q) in possible:
+						c+=self.profit(self.move(self.copy(cell),x,y,p,q),color*-1,pcolor,npforce,noforce,depth-1)
+					
+					return c+(npforce-pforce+oforce-noforce)*100*depth*100
+			break
+		
+		
+		
+		c=0
+		for (x,y) in self.force(color,cell):
+			possible=self.coverage(x,y,cell)
+			
+			for (p,q) in possible:
+				c+=self.profit(self.move(self.copy(cell),x,y,p,q),color*-1,pcolor,npforce,noforce,depth-1)
+		
+				
+		return c+(npforce-pforce+oforce-noforce)*100*depth*100
+					
+					
+	def checkmate(self,cell,color):
+		for (x,y) in self.force(color,cell):
+			
+			if cell[x][y].rank==1:
+				
+				if not self.safe(x,y,cell,color):
+					
+					if len(self.coverage(x,y,cell))==0:
+
+ 						return True
+		
+	
+	
+	def bestpos(self,cell,color,pcolor,depth):	
+		bestprofit=-99999
+		
+		best=None
+		npforce=self.power(color,cell)
+		noforce=self.power(color*-1,cell)
+		for (x,y) in self.force(color,cell):
+			
+			if cell[x][y].rank==1:
+				
+				if not self.safe(x,y,cell,color):
+					
+					if len(self.coverage(x,y,cell))==0:
+						
+						return None
+						
+					possible=self.coverage(x,y,cell)
+					for (p,q) in possible:
+						c=self.profit(self.move(self.copy(cell),x,y,p,q),color*-1,pcolor,npforce,noforce,depth-1)
+						if c>bestprofit:
+							best=self.move(self.copy(cell),x,y,p,q)
+							bestprofit=c
+					return best
+				break
+				
+		
+			
+		bestprofit=-99999
+		for (x,y) in self.force(color,cell):
+			possible=self.coverage(x,y,cell)
+		
+			
+			for (p,q) in possible:
+				 
+				c=self.profit(self.move(self.copy(cell),x,y,p,q),color*-1,pcolor,npforce,noforce,depth-1)
+				print (x,y), (p,q), c
+				
+				if c>=bestprofit:
+					best=self.move(self.copy(cell),x,y,p,q)
+					bestprofit=c
+					
+		return best
+	def bestposition(self,cell,color,depth):
+		return self.bestpos(cell,color,color,depth)
+		
